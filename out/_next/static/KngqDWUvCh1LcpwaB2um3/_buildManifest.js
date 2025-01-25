8f8425801a62e46ad814a4ae85cf2522827c65c3
jigpx <anditsjig@gmail.com>
2025-01-24 21:01:08 -0500

    Rebuild and deploy to GitHub Pages

---
+++ out/_next/static/KngqDWUvCh1LcpwaB2um3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-e639858ac40c3812.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
